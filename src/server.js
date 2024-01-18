32283e0bf45398e74fffbcab7d958bb794405f73
Akmal Dayan <akmaldyn02@gmail.com>
2024-01-18 13:47:37 +0700

    restructure with hapi plugin

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
- 
+
 const init = async () => {
   const notesService = new NotesService();
   const server = Hapi.server({
@@ -13,16 +13,16 @@ const init = async () => {
       },
     },
   });
- 
+
   await server.register({
     plugin: notes,
     options: {
       service: notesService,
     },
   });
- 
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
+
 init();
