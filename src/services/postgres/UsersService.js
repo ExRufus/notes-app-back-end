36b00c74279a023cbf5a161af6ae2bc750473511
Akmal Dayan <akmaldyn02@gmail.com>
2024-01-24 23:09:55 +0700

    done

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -2,6 +2,8 @@ const { nanoid } = require("nanoid");
 const { Pool } = require("pg");
 const bcrypt = require('bcrypt');
 const InvariantError = require("../../exceptions/InvariantError");
+const NotFoundError = require("../../exceptions/NotFoundError");
+const AuthenticationError = require("../../exceptions/AuthenticationError");
 
 class UsersService {
   constructor() {
@@ -14,6 +16,7 @@ class UsersService {
 
     // TODO: Bila verifikasi lolos, maka masukkan user baru ke database.
     const id = `user-${nanoid(16)}`;
+    // sebelum dimasukkan kedalam database password yang di input oleh user di hash terlebih dahulu
     const hashedPassword = await bcrypt.hash(password, 10);
     const query = {
       text: 'INSERT INTO users VALUES($1, $2, $3, $4) RETURNING id',
@@ -58,6 +61,33 @@ class UsersService {
 
     // Selain itu, kembalikan fungsi getUserById dengan nilai user yang didapat pada result.rows[0].
     return result.rows[0];
+  }
+
+  // Fungsi yang dimaksud adalah verifikasi apakah kredensial atau username dan password yang dikirimkan oleh pengguna benar atau tidak.
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('Kredensial yang anda berikan salah');
+    }
+
+    // Untuk nilai password, kita tampung ke variabel hashe Password ya biar tidak ambigu dengan variabel password di parameter.
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    // Sekarang kita bisa evaluasi variabel match, jika hasilnya false, bangkitkan eror AuthenticationError dengan pesan ‘Kredensial yang Anda berikan salah’. Jika hasilnya true, kembalikan dengan nilai id user.
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    // Nilai user id tersebut nantinya akan digunakan dalam membuat access token dan refresh token.
+    return id;
   }
 }
 
---
+++ src/tokenize/TokenManager.js
@@ -0,0 +1,23 @@
+const Jwt = require('@hapi/jwt');
+const InvariantError = require('../exceptions/InvariantError');
+
+const TokenManager = {
+  // Buat properti fungsi generateAccessToken yang menerima satu parameter yakni payload.
+  // generateAccess(payload) { // Parameter payload merupakan objek yang disimpan ke dalam salah satu artifacts JWT. Biasanya objek payload berisi properti yang mengindikasikan identitas pengguna, contohnya user id.
+  //   return Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY); // Fungsi generate menerima dua parameter, yang pertama adalah payload dan kedua adalah secretKey.
+  // },
+    generateAccessToken: (payload) => Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
+    generateRefreshToken: (payload) => Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
+    // Untuk men-decoded token, gunakan fungsi Jwt.token.decode dan fungsi tersebut akan mengembalikan artifacts.  
+    verifyRefreshToken: (refreshToken) => {
+      try {
+        const artifacts = Jwt.token.decode(refreshToken);
+        // Fungsi verifySignature ini akan mengecek apakah refresh token memiliki signature yang sesuai atau tidak. Jika hasil verifikasi sesuai, fungsi ini akan lolos. Namun bila tidak, maka fungsi ini akan membangkitkan eror.
+        Jwt.token.verifySignature(artifacts, process.env.REFRESH_TOKEN_KEY); // hanya menerima token dalam bentuk artifacts atau token yang sudah di-decoded
+      } catch (error) {
+        throw new InvariantError('Refresh token tidak valid');
+      }
+    },
+};
+
+module.exports = TokenManager;
